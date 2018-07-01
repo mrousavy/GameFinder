215d9cb1f4858b048fd54a15b766fad0018a4873
mrousavy <mrousavy@donotreply.github.com>
2018-07-01 18:53:24 +0200

    Create Login View

--- GameFinder/Login/LoginViewModel.cs
+++ GameFinder/Login/LoginViewModel.cs
@@ -4,5 +4,21 @@ namespace GameFinder.Login
 {
     public class LoginViewModel : ViewModel
     {
+        private string _apiKey;
+
+        public string ApiKey
+        {
+            get => _apiKey;
+            set => Set(ref _apiKey, value);
+        }
+
+
+        private string _userId;
+
+        public string UserId
+        {
+            get => _userId;
+            set => Set(ref _userId, value);
+        }
     }
 }
